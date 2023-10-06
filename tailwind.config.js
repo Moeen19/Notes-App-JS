20b68f046d6f3c8ef6e25571cfafa94c0587e12a
moeen <moeen12yt@gmail.com>
2023-10-06 21:16:03 +0500

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./Project/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '100px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+      'lm': '991px',
+      'lg': '1100px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1180px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+
     extend: {},
   },
   plugins: [],
